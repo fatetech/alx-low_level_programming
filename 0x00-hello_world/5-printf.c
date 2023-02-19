d6501d87815554469c81507a45c67f34022894a7
Fate <stephalex222@gmail.com>
2023-02-19 11:36:34 -0800

    5-print.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - writing a C program that prints a line with prinf
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
