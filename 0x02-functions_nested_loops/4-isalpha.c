d45eb70eb5a05c0e332447a75162b3c01c6792fd
Fate <stephalex222@gmail.com>
2023-02-22 15:29:25 -0800

    updating tasks

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetica char
+ * @c: the char to be checked
+ * Return: 1 if c is a letter, otherwise 0.
+ */
+int _isalpha(int c);
+{
+	((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+}
