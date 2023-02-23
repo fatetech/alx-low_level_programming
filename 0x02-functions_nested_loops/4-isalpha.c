a647ed5b4908e502315ba395fa93d50291c0c501
Fate <stephalex222@gmail.com>
2023-02-22 18:42:03 -0800

    updating

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: the char to be checked
  * Return: 1 if c is a letter, otherwise 0.
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
