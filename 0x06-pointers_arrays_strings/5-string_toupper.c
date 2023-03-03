1f4d1b9c7dd44526daba6897c67716b86be6411b
Fate <stephalex222@gmail.com>
2023-03-02 19:17:05 -0800

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,11 +12,9 @@ char *string_toupper(char *n)
 	i = 0;
 	while (n[i] != '\0')
 	{
-		if (n[i] >= 'a' && n[i] <= 'z')
-		{
+		if (n[i} >= 'a' && n[i] <= 'z')
 			n[i] = n[i] - 32;
 			i++;
-		}
-		return (n)
 	}
+return (n);
 }
