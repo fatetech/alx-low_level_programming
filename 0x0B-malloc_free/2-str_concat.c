034116fcbca128c04c16e2fe0cd665020ec72343
Fate <stephalex222@gmail.com>
2023-03-13 14:06:34 -0700

    tasks

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,13 +9,14 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
-	int i, ci;
+	int i;
+	int ci;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	i, ci = 0;
+	i = ci = 0;
 	while (s1[i] != '\0')
 		i++;
 	while (s2[ci] != '\0')
@@ -23,7 +24,7 @@ char *str_concat(char *s1, char *s2)
 	conct = malloc(sizeof(char) * (i + ci + 1));
 	if (conct == NULL)
 		return (NULL);
-	i, ci = 0;
+	i = ci = 0;
 		while (s1[i] != '\0')
 		{
 			conct[i] = s1[i];
