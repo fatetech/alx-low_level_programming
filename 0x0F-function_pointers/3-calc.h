9fa755d17843c3bb5cadc66f35942018fb459710
Fate <stephalex222@gmail.com>
2023-03-22 17:53:47 -0700

    tasks

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,14 +1,19 @@
-#include "3-calc.h"
+#ifndef CALC_H
+#define CALC_H
+/**
+ * struct op - A struct op.
+ * @op: The operator.
+ * @f: The associated function.
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+}op_t;
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-
-/**
-	 * op_add - Returns the sum of two numbers.
-	 * @a: The first number.
-	 * @b: The second number.
-	 *
-	 * Return: The sum of a and b.
-	 */
+int (*get_op_func(char *s))(int, int);
+#endif
